551612c2412f54c9fc19bf3997cea09643d46334
Shashwat  Singh <shashwat.singh.31.1@mountblue.tech>
2025-04-15 22:11:04 +0530

    add assets

---
+++ src/assets/instructions.js
@@ -0,0 +1,29 @@
+const instructions = {
+  smallScreen: [
+    {
+      question: "Time Per Question",
+      answer: "30 sec",
+    },
+    {
+      question: "Total Questions",
+      answer: 10,
+    },
+  ],
+
+  largeScreen: [
+    {
+      question: "Time Per Question",
+      answer: "30 sec",
+    },
+    {
+      question: "Total Questions",
+      answer: 10,
+    },
+    {
+      question: "Coins",
+      answer: "20 Coins",
+    },
+  ],
+};
+
+export default instructions;
